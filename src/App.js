69840e438faf428e2027b1282eff0dad829c8d89
jam5322 <jam5322@g.rit.edu>
2021-11-14 16:08:35 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+const current_rek_snapshot = logo;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Current Rekognition Snapshot:</h1>
+        <img src={current_rek_snapshot} className="Rekognition-Snaptshot" alt="latest AWS Rekognition Snapshot" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Img filename: <code>insert filename of image here if desired</code>
+        </p>
+        <p>
+          Img timestamp: <code>insert timestamp of image here if desired</code>
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="AWS-Management-Console-link"
+          href="https://aws.amazon.com/console"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          AWS Management Console
         </a>
       </header>
     </div>
